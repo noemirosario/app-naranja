6468f1200667ca5513fab509de4c455bf1f59b54
Noemi R <noemi_1212rosario@hotmail.com>
2023-03-18 21:13:22 -0600

    first commit

---
+++ src/componentes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import colors from '../utils/colors';
+
+export default function Home(){
+    return(
+        <ScrollView>
+            <View>
+                <Button>
+                    hola
+                </Button>
+            </View>
+        </ScrollView>
+    );
+}
---
+++ src/utilidades/colors.js
@@ -0,0 +1,9 @@
+export default {
+    COLOR_MORADO: '#702283',
+    COLOR_LILA: '#962071',
+    COLOR_VERDE: '#76B39D',
+    
+    COLOR_AMARILLO: '#FDB44B',
+    COLOR_BLANCO: '#FFF',
+
+}
